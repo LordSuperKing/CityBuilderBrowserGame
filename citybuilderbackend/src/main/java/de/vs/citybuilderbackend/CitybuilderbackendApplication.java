b80889c14d415219eb4cce22275948cca4a1aa2a
steick,viktor(SE-A/35) <viktor.steick@volkswagen.de>
2024-04-03 14:57:14 +0200

    initital commit

---
+++ citybuilderbackend/src/main/java/de/vs/citybuilderbackend/CitybuilderbackendApplication.java
@@ -0,0 +1,13 @@
+package de.vs.citybuilderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitybuilderbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitybuilderbackendApplication.class, args);
+	}
+
+}
---
+++ citybuilderbackend/src/test/java/de/vs/citybuilderbackend/CitybuilderbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.vs.citybuilderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitybuilderbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
